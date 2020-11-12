a970e8d4eb63dc4fef16ad1870ef0dd36d61f004
kasiwa-mesi <italynataly@outlook.jp>
2020-11-12 15:16:37 +0900

    prepare for ogp image

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
+const baseName = 'ミチノリ'
+const baseDesc = '継続を習慣づける管理アプリ'
+const baseUrl = 'https://neta-tube.web.app'
+
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
@@ -9,6 +13,20 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { hid: 'og:site_name', property: 'og:site_name', content: baseName },
+      { hid: 'og:type', property: 'og:type', content: 'article' },
+      { hid: 'og:url', property: 'og:url', content: baseUrl },
+      { hid: 'og:title', property: 'og:title', content: baseName },
+      { hid: 'og:description', property: 'og:description', content: baseDesc },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: baseUrl + '/image.png'
+      },
+      { property: 'article:publisher', content: 'FacebookURL' },
+      { property: 'fb:app_id', content: 'FacebookAppID' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
