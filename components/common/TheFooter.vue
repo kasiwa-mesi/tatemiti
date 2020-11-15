8f8be65efbd5c4899c517e04e41d5c1b560b57f5
kasiwa-mesi <italynataly@outlook.jp>
2020-11-15 18:15:54 +0900

    add agreement page and fix text in footer]

--- components/common/TheFooter.vue
+++ components/common/TheFooter.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-full bg-green-500 py-8 text-white text-center">
-    created by
-    <a
-      href="https://twitter.com/kasiwa9494"
-      target="_blank"
-      class="underline"
-    >
-      @kasiwa9494
-    </a>
+    <p class="font-semibold text-center pb-5">©︎ タテミチ</p>
+    <div class="flex information-wrap pb-4">
+      <p class="font-mono">開発・運営</p>
+      <a target="_blank" class="ml-2 underline" href="https://twitter.com/kasiwa9494">@kasiwa9494</a>
+    </div>
+    <div class="information-wrap flex pb-4">
+      <a class="underline" href="/agreement">利用規約</a>
+    </div>
   </div>
 </template>
 
@@ -17,3 +17,19 @@ export default Vue.extend({
   name: 'TheFooter'
 })
 </script>
+
+<style scoped>
+.information-wrap {
+  margin-left: 43%;
+}
+@media screen and (max-width: 768px) {
+  .information-wrap {
+    margin-left: 38%;
+  }
+}
+@media screen and (max-width: 425px) {
+  .information-wrap {
+    margin-left: 25%;
+  }
+}
+</style>
